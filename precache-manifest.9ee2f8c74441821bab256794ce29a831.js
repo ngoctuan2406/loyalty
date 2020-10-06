a292d5b401945dab7b93d910d28ae5f7d5f80776
abc <famsun01@gmail.com>
2020-10-06 14:18:56 +0700

    Updates

---
+++ precache-manifest.9ee2f8c74441821bab256794ce29a831.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ec5fd10a899845f3108dc600f89250",
+    "url": "/loyalty/index.html"
+  },
+  {
+    "revision": "dc730e19234e2af7c80d",
+    "url": "/loyalty/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f82e46cee2fea9879db",
+    "url": "/loyalty/static/js/2.177cfbc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/loyalty/static/js/2.177cfbc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc730e19234e2af7c80d",
+    "url": "/loyalty/static/js/main.72b01a8e.chunk.js"
+  },
+  {
+    "revision": "37afe1128d675fe43add",
+    "url": "/loyalty/static/js/runtime-main.75720530.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/loyalty/static/media/logo.5d5d9eef.svg"
+  }
+]);
