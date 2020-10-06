d3c30341f99aaa4d8fe661275603d2aebf13e269
TuanNN <>
2020-10-06 16:37:51 +0700

    build

---
+++ build/precache-manifest.20b8d5d513b3d5f321f676248cdd194e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32830e3670c8e52de9d41af160f381f",
+    "url": "/loyalty/index.html"
+  },
+  {
+    "revision": "5289ee449aa0e6f2190f",
+    "url": "/loyalty/static/css/main.3b493a9f.chunk.css"
+  },
+  {
+    "revision": "fa7479766c51a0a4d02e",
+    "url": "/loyalty/static/js/2.53b7189a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/loyalty/static/js/2.53b7189a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5289ee449aa0e6f2190f",
+    "url": "/loyalty/static/js/main.20ee9081.chunk.js"
+  },
+  {
+    "revision": "37afe1128d675fe43add",
+    "url": "/loyalty/static/js/runtime-main.75720530.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/loyalty/static/media/logo.5d5d9eef.svg"
+  }
+]);
